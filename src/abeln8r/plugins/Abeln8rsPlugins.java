aa0a74db66dc9c7dbf10f60b2c8a00745b95b886
Abeln8r <>
2012-04-10 15:33:16 -0400

    Import Clean Up

--- src/abeln8r/plugins/Abeln8rsPlugins.java
+++ src/abeln8r/plugins/Abeln8rsPlugins.java
@@ -1,14 +1,10 @@
 package abeln8r.plugins;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event.Priority;
-import org.bukkit.event.Event.Type;
-import org.bukkit.event.EventPriority;
-import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class Abeln8rsPlugins extends JavaPlugin
