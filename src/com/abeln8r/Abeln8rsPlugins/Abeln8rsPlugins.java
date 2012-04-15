5c518bd850a9e326180c433243c116f29050bd14
abeln8r <>
2012-04-15 16:43:24 -0400

    NullPointer

--- src/com/abeln8r/Abeln8rsPlugins/Abeln8rsPlugins.java
+++ src/com/abeln8r/Abeln8rsPlugins/Abeln8rsPlugins.java
@@ -8,7 +8,7 @@ public class Abeln8rsPlugins extends JavaPlugin
     
     public void onEnable()
     {
-        this.getCommand("ap").setExecutor(this);
+        this.getCommand("ap").setExecutor(new Abeln8rsPluginCommand(this));
     }
  
     public void onDisable()
